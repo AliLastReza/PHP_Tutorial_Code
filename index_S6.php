5777dabdabe3c36d86180a45362813c81a0110e1
Mohammad Ali Reza <alilastreza@pm.me>
2023-05-01 11:58:35 -0700

    Code of "6: What Are Internal Functions in PHP | PHP Tutorial | Learn PHP Programming | PHP for Beginners"

---
+++ index_S6.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Code of " 6: What Are Internal Functions in PHP | PHP Tutorial | Learn PHP Programming | PHP for Beginners "</title>
+</head>
+<body>
+    <?php
+        echo "strlen: ".strlen("Hi Ali");
+        echo "<br />";
+        echo "str_word_count: ".str_word_count("Hi Ali");
+        echo "<br />";
+        echo "strrev: ".strrev("Hi Ali");
+        echo "<br />";
+        echo "strpos: ".strpos("Hi Ali", "Ali");
+        echo "<br />";
+        echo "str_replace: ".str_replace("Ali", "Mohammad", "Hi Ali");
+     ?>
+</body>
+</html>
