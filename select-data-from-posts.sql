553f317a11499414fa1f377e6779e9c74353fc40
Mohammad Ali Reza <alilastreza@pm.me>
2023-05-08 17:36:58 -0700

    Code of "32: Select Data From MySQL Database | PHP Tutorial | Learn PHP Programming | PHP for Beginners"

---
+++ select-data-from-posts.sql
@@ -0,0 +1,11 @@
+select subject from posts;
+
+select * from posts;
+
+select * from posts where id='1';
+
+select * from posts where id='1' AND subject='This is the subject';
+
+select * from posts where id='1' AND subject='Thif is the subject';
+
+select * from posts where id='1' OR subject='This is the subject';
