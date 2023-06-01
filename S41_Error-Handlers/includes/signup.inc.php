c6ecabe3d9112cad21dcf2f53c361fc97230522a
Mohammad Ali Reza <alilastreza@pm.me>
2023-06-01 13:45:37 -0700

    Code of "41: What Are Error Handlers in PHP"

---
+++ S41_Error-Handlers/includes/signup.inc.php
@@ -0,0 +1,33 @@
+<?php
+
+if (!isset($_POST['submit'])) {
+    header("Location: ../index.php?signup=error");
+} else {
+    include_once 'dbh.inc.php';
+    $first = mysqli_real_escape_string($conn, $_POST['first']);
+    $last = mysqli_real_escape_string($conn, $_POST['last']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $uid = mysqli_real_escape_string($conn, $_POST['uid']);
+    $pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
+
+    if (empty($first) || empty($last) || empty($email) || empty($uid) || empty($pwd)) {
+        header("Location: ../index.php?signup=empty");
+    } else {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            header("Location: ../index.php?signup=invalid_email");
+        } else {
+            $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd)
+            VALUES (?, ?, ?, ?, ?);";
+            $stmt = mysqli_stmt_init($conn);
+            if (!mysqli_stmt_prepare($stmt, $sql)) {
+                echo "SQL statement failed";
+            } else {
+                mysqli_stmt_bind_param($stmt, "sssss", $first, $last, $email, $uid, $pwd);
+                mysqli_stmt_execute($stmt);
+            }
+            header("Location: ../index.php?signup=success");
+        }
+    }
+}
+
+
