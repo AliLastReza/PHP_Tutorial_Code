522d223cbbfadfbc1b073d5914e905bb64dce05c
Mohammad Ali Reza <ja4ari@gmail.com>
2023-06-21 17:47:51 -0600

    Minor change in S43.../header.php

--- S43_Login-System/header.php
+++ S43_Login-System/header.php
@@ -8,7 +8,7 @@ session_start();
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Code of "How To Create A Login System In PHP For Beginners"</title>
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
