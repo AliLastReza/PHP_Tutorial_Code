d588c89f957b7390369f2ef47d63d621ee25aec6
Mohammad Ali Reza <ja4ari@gmail.com>
2023-06-27 13:29:42 -0600

    Modify 1 of the insert queries in S57

--- S57_Create-A-Search-Field/insert-samples-into-posts.sql
+++ S57_Create-A-Search-Field/insert-samples-into-posts.sql
@@ -1,59 +1,7 @@
 INSERT INTO posts (title, content, author, createdAt)
             VALUES ('Sample blog post', "This blog post shows a few different types of content that’s supported and styled with Bootstrap. Basic typography, lists, tables, images, code, and more are all supported as expected.
 
-This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.
-Blockquotes
-
-This is an example blockquote in action:
-
-    Quoted text goes here.
-
-This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.
-Example lists
-
-This is some additional paragraph placeholder content. It's a slightly shorter version of the other highly repetitive body text used throughout. This is an example unordered list:
-
-    First list item
-    Second list item with a longer description
-    Third list item to close it out
-
-And this is an ordered list:
-
-    First list item
-    Second list item with a longer description
-    Third list item to close it out
-
-And this is a definition list:
-
-HyperText Markup Language (HTML)
-    The language used to describe and define the content of a Web page
-Cascading Style Sheets (CSS)
-    Used to describe the appearance of Web content
-JavaScript (JS)
-    The programming language used to build advanced Web sites and applications
-
-Inline HTML elements
-
-HTML defines a long list of available inline tags, a complete list of which can be found on the Mozilla Developer Network.
-
-    To bold text, use <strong>.
-    To italicize text, use <em>.
-    Abbreviations, like HTML should use <abbr>, with an optional title attribute for the full phrase.
-    Citations, like — Mark Otto, should use <cite>.
-    Deleted text should use <del> and inserted text should use <ins>.
-    Superscript text uses <sup> and subscript text uses <sub>.
-
-Most of these elements are styled by browsers with few modifications on our part.
-Heading
-
-This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.
-Sub-heading
-
-This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.
-
-Example code block
-
-This is some additional paragraph placeholder content. It's a slightly shorter version of the other highly repetitive body text used throughout.", "Mark", "2023-06-26 13:01");
+This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.", "Mark", "2023-06-26 13:01");
 
 
 INSERT INTO posts (title, content, author, createdAt)
